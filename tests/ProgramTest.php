0d2c6366081c22f42f9d8cf5816fb206e8277bf4
ha1t <halt.feits@gmail.com>
2012-02-08 23:37:34 +0900

    Travis-CIの設定入れたけど、DB処理含むテストは一体どうすればいいのか

---
+++ tests/ProgramTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ *
+ */
+class ProgramTest extends PHPUnit_Framework_TestCase
+{
+    public function testSearch() 
+    {
+        //$rows = Program::search('', array()); 
+        //$this->assertTrue(is_array($rows)); 
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ *
+ */
+
+error_reporting( E_ALL | E_STRICT );
+
+require_once dirname(dirname(__FILE__)) . '/config.php'; 
