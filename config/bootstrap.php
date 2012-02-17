75c909f32cbad1b7e2b2d968f80697bb0f246499
ha1t <halt.feits@gmail.com>
2012-02-17 16:37:56 +0900

    bootstrap利用

---
+++ config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+// bootstrap
+require_once dirname(dirname(__FILE__)) . '/database.php';
+require_once dirname(dirname(__FILE__)) . '/models/model.php';
+require_once dirname(dirname(__FILE__)) . '/models/category.php';
+require_once dirname(dirname(__FILE__)) . '/models/channel.php';
+require_once dirname(dirname(__FILE__)) . '/models/program.php';
+require_once dirname(dirname(__FILE__)) . '/models/reserve.php';
+require_once dirname(dirname(__FILE__)) . '/models/keyword.php';
+require_once dirname(dirname(__FILE__)) . '/models/recorder_service.php';
+
+require_once dirname(dirname(__FILE__)) . '/models/channel_master.php';
+
+require_once dirname(dirname(__FILE__)) . '/Smarty/Smarty.class.php';
--- config_travisci.php
+++ config_travisci.php
@@ -4,20 +4,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_ATTR_TIMEOUT', 3);
 
-// @TODO 後で分割する
-// bootstrap
-require_once dirname(__FILE__) . '/database.php';
-require_once dirname(__FILE__) . '/models/model.php';
-require_once dirname(__FILE__) . '/models/category.php';
-require_once dirname(__FILE__) . '/models/channel.php';
-require_once dirname(__FILE__) . '/models/program.php';
-require_once dirname(__FILE__) . '/models/reserve.php';
-require_once dirname(__FILE__) . '/models/keyword.php';
-require_once dirname(__FILE__) . '/models/recorder_service.php';
-
-require_once dirname(__FILE__) . '/models/channel_master.php';
-
-require_once dirname(__FILE__) . '/Smarty/Smarty.class.php';
+require_once dirname(__FILE__) . '/config/bootstrap.php';
 
 // 録画モード（option）
 $RECORD_MODE = array(
