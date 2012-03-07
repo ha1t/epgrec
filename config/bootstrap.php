404b36c5c9500c92ee55f3d226b727c6b8c1d27d
ha1t <halt.feits@gmail.com>
2012-03-07 23:35:05 +0900

    autoload追加

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 // bootstrap
 require_once dirname(__FILE__) . '/database.php';
-require_once dirname(dirname(__FILE__)) . '/models/model.php';
 require_once dirname(dirname(__FILE__)) . '/models/category.php';
 require_once dirname(dirname(__FILE__)) . '/models/channel.php';
 require_once dirname(dirname(__FILE__)) . '/models/program.php';
@@ -12,3 +11,17 @@ require_once dirname(dirname(__FILE__)) . '/models/recorder_service.php';
 require_once dirname(dirname(__FILE__)) . '/models/channel_master.php';
 
 require_once dirname(dirname(__FILE__)) . '/Smarty/Smarty.class.php';
+
+// autoload
+spl_autoload_register(function($name) {
+    $filename = Inflector::underscore($name) . '.php';
+    if (strpos($name, 'Controller') !== false) {
+        require CONTROLLERS_DIR . $filename;
+    } elseif (strpos($name, 'Master') !== false) {
+        require MASTER_DIR . $filename;
+    } else {
+        if (file_exists(MODELS_DIR . $filename)) {
+            require MODELS_DIR . $filename;
+        }
+    }
+});
