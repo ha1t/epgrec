7666dcb4655e2a9e44dd7674f77b5431dc17a7ab
ha1t <halt.feits@gmail.com>
2012-03-08 00:02:50 +0900

    InstallControllerを仮追加

---
+++ app/controllers/install_controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ *
+ */
+
+class InstallController extends AppController
+{
+    public function step1() 
+    {
+    }
+
+    public function step2() 
+    {
+    }
+
+    public function step3() 
+    {
+    }
+
+    public function step4() 
+    {
+    }
+
+    public function step5() 
+    {
+    }
+
+}
