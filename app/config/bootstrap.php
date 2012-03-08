e7c03b2bd16b1ae8bc48f253427284886b0df194
ha1t <halt.feits@gmail.com>
2012-03-08 15:43:26 +0900

    autoloadを5.2に対応する

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -12,6 +12,23 @@ require_once APP_DIR.'app_exception.php';
 require_once ROOT_DIR . '/Smarty/Smarty.class.php';
 
 // autoload
+$code = '
+$filename = Inflector::underscore($name) . ".php";
+if (strpos($name, "Controller") !== false) {
+    require CONTROLLERS_DIR . $filename;
+} elseif (strpos($name, "Master") !== false) {
+    require MASTER_DIR . $filename;
+} else {
+    if (file_exists(MODELS_DIR . $filename)) {
+        require MODELS_DIR . $filename;
+    }
+}
+';
+$func = create_function('$name', $code);
+
+spl_autoload_register($func);
+
+/*
 spl_autoload_register(function($name) {
     $filename = Inflector::underscore($name) . '.php';
     if (strpos($name, 'Controller') !== false) {
@@ -24,3 +41,4 @@ spl_autoload_register(function($name) {
         }
     }
 });
+ */
