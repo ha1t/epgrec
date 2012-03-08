18067932cef39d3b63f5304433cf2ab04424873d
ha1t <halt.feits@gmail.com>
2012-03-08 11:08:27 +0900

    autoloadを利用するようにしたので、requireを削除

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,25 +1,15 @@
 <?php
 // bootstrap
-require_once dirname(__FILE__) . '/database.php';
 
-/*
-require_once dirname(dirname(__FILE__)) . '/models/category.php';
-require_once dirname(dirname(__FILE__)) . '/models/channel.php';
-require_once dirname(dirname(__FILE__)) . '/models/program.php';
-require_once dirname(dirname(__FILE__)) . '/models/reserve.php';
-require_once dirname(dirname(__FILE__)) . '/models/keyword.php';
-require_once dirname(dirname(__FILE__)) . '/models/recorder_service.php';
+define('MASTER_DIR', APP_DIR . 'master/'); 
 
-require_once dirname(dirname(__FILE__)) . '/models/channel_master.php';
- */
+require_once CONFIG_DIR . 'database.php';
 
 require_once APP_DIR.'app_controller.php';
 require_once APP_DIR.'app_model.php';
 require_once APP_DIR.'app_exception.php';
 
 require_once ROOT_DIR . '/Smarty/Smarty.class.php';
-
-define('MASTER_DIR', APP_DIR . 'master/'); 
 
 // autoload
 spl_autoload_register(function($name) {
